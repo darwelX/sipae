388f8ee23f90840d6b7b59e7844af73668a0b641
darwelX <darwel34@gmail.com>
2013-11-15 15:31:31 -0430

    se agregaron las demas clases que componen el modelo del modulo de inscripciones y se registraron sus respectivos componentes

--- inscripcion/admin.py
+++ inscripcion/admin.py
@@ -6,9 +6,25 @@ Created on 15/11/2013
 from inscripcion.models import Parentesco
 from inscripcion.models import Nacionalidad
 from inscripcion.models import Padre
+from inscripcion.models import Madre
+from inscripcion.models import Representante
+from inscripcion.models import Escolaridad
+from inscripcion.models import Materia
+from inscripcion.models import Anio
+from inscripcion.models import Seccion
+from inscripcion.models import Estudiante
+from inscripcion.models import Inscripcion
 from django.contrib import admin
 
 
 admin.site.register(Parentesco)
 admin.site.register(Nacionalidad)
 admin.site.register(Padre)
+admin.site.register(Madre)
+admin.site.register(Representante)
+admin.site.register(Escolaridad)
+admin.site.register(Materia)
+admin.site.register(Anio)
+admin.site.register(Seccion)
+admin.site.register(Estudiante)
+admin.site.register(Inscripcion)
