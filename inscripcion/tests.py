f311f4361e8043ca866c6e26943b021412c046bb
darwelX <darwel34@gmail.com>
2013-11-14 21:32:37 -0430

    se cambio el nombre del paquete fuente de los modelos

--- inscripcion/tests.py
+++ inscripcion/tests.py
@@ -7,7 +7,7 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-from books.models import *
+from inscripcion.models import *
 import datetime
 
 class SimpleTest(TestCase):
