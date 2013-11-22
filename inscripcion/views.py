5a9746a68111e6160a6e11a5defd642489c6fef7
darwelX <darwel34@gmail.com>
2013-11-22 19:06:33 -0430

    se agrego la logica del proceso de inscripcion

--- inscripcion/views.py
+++ inscripcion/views.py
@@ -5,3 +5,6 @@ from django.template import RequestContext
 def inicio(request):
   mensaje="hola mundo"
   return render_to_response('index.html',locals(),RequestContext(request))
+  
+def inscripcion(request):
+    return render_to_response('inscripcion/inscripcion.html',locals(),RequestContext(request))
