c89a7fa71a1421b436dcf80b826a360d1f4c40f3
darwelX <darwel34@gmail.com>
2013-11-25 19:58:36 -0430

    se agrego la logica que permite buscar un estudiante por nacionalidad y cedula

--- inscripcion/views.py
+++ inscripcion/views.py
@@ -1,10 +1,24 @@
 # Create your views here.
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
+from inscripcion.models import *
+
 
 def inicio(request):
   mensaje="hola mundo"
   return render_to_response('index.html',locals(),RequestContext(request))
   
 def inscripcion(request):
+    nacionalidades = Nacionalidad.objects.all()
+    estudiante = Estudiante()
+    
+    if request.method == 'POST':
+        
+        if request.POST['procesarButton'] == 'Busccar':
+            n = Nacionalidad.objects.get(pk=int(request.POST['nacionalidad']))
+            e = list(Estudiante.objects.filter(nacionalidad = n, cedula=request.POST['cedula']))
+            if(len(e) > 0):
+                estudiante = e[0]
+            print type(estudiante)
+            
     return render_to_response('inscripcion/inscripcion.html',locals(),RequestContext(request))
