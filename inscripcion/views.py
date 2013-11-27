2f71333c823b2eb5881e2bbff5294ac63a5f9030
darwelX <darwel34@gmail.com>
2013-11-26 20:39:30 -0430

    se agrego la variable para saber si se encontro el estudiante asi como la variable que maneja mensajes de error y lña variable que representa el formulario de un estudiante, se cambio la condicion de validacion de la nacionalidad y el manejo de mensaje de error, se agrego la logica para el boton siguiente asi como el manejo de un estudiante nuevo como existente

--- inscripcion/views.py
+++ inscripcion/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from inscripcion.models import *
-
+from inscripcion.forms import EstudianteForm
 
 def inicio(request):
   mensaje="hola mundo"
@@ -11,14 +11,34 @@ def inicio(request):
 def inscripcion(request):
     nacionalidades = Nacionalidad.objects.all()
     estudiante = Estudiante()
+    estudiante_encontrado = False
+    mensaje_error = ""
+    formEstudiante = EstudianteForm()
     
     if request.method == 'POST':
         
         if request.POST['procesarButton'] == 'Busccar':
-            n = Nacionalidad.objects.get(pk=int(request.POST['nacionalidad']))
-            e = list(Estudiante.objects.filter(nacionalidad = n, cedula=request.POST['cedula']))
-            if(len(e) > 0):
-                estudiante = e[0]
             
+            if request.POST['nacionalidad'] != '':
+                n = Nacionalidad.objects.get(pk=int(request.POST['nacionalidad']))
+                e = list(Estudiante.objects.filter(nacionalidad = n, cedula=request.POST['cedula']))
+                if(len(e) > 0):
+                    estudiante = e[0]
+                    estudiante_encontrado = True
+                else:
+                    mensaje_error="Estudiante no encontrado"
             
+        if request.POST['procesarButton'] == 'Siguiente':
+                        
+            if request.POST['id_estudiante'] == 'nuevo':
+                formEstudiante = EstudianteForm(request.POST)
+                
+                if formEstudiante.is_valid():
+                    print 'continuar'
+                else:
+                    print 'no puede continuar'
+            else:
+                estudiante_encontrado = True
+                estudiante = Estudiante.objects.get(pk=int(request.POST['id_estudiante']))
+                
     return render_to_response('inscripcion/inscripcion.html',locals(),RequestContext(request))
